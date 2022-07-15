41460dc27e6dbcbabdf7d7ab321007f24fa6773e
Free-Hugs <kkdo236@gmail.com>
2022-07-15 23:44:12 +0200

    Add must watch activity

--- app/src/main/java/com/example/movieranking/NotWatched.java
+++ app/src/main/java/com/example/movieranking/NotWatched.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Ranking extends AppCompatActivity {
+public class NotWatched extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ranking);
+        setContentView(R.layout.activity_not_watched);
     }
 }
