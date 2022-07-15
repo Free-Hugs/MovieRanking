d9a3a5492718d00ccf6e40faacf277c841ed367f
Free-Hugs <kkdo236@gmail.com>
2022-07-15 23:57:23 +0200

    Linking main activity's buttons to other activities

--- app/src/main/java/com/example/movieranking/MainActivity.java
+++ app/src/main/java/com/example/movieranking/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.movieranking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ranking = findViewById(R.id.rated);
+        ranking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Ranking.class);
+                startActivity(intent);
+            }
+        });
+
+        Button mustWatch = findViewById(R.id.notViewed);
+        mustWatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NotWatched.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
